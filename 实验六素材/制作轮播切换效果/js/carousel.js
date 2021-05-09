d03c1e35b57ca68b6f61ee2068a3bf1d6816c49f
skydyboy <superboyyy6@gmail.com>
2021-05-09 14:52:28 +0800

    JavaScript

--- 实验六素材/制作轮播切换效果/js/carousel.js
+++ 实验六素材/制作轮播切换效果/js/carousel.js
@@ -1,3 +1,29 @@
 $(document).ready(function(){
-    
+    var index = 1;
+    $(".adver").mouseover(function () {
+        $(".arrowLeft").show();
+        $(".arrowRight").show();
+    })
+    $(".adver").mouseout(function () {
+        $(".arrowLeft").hide();
+        $(".arrowRight").hide();
+    })
+    $(".arrowRight").click(function () {
+        index++;
+        if (index > 3) {
+            index = 3;
+            alert("已经是最后一张图片了");
+        }
+        $(".adver").css("background", "url(images/"+index+".jpg)");
+        $("li:nth-of-type("+index+")").css("background","orange").siblings().css("background", "#333333");
+    })
+    $(".arrowLeft").click(function () {
+        index--;
+        if (index < 1) {
+            index = 1;
+            alert("这是第一张图片了");
+        }
+        $(".adver").css("background", "url(images/"+index+".jpg)");
+        $("li:nth-of-type("+index+")").css("background","orange").siblings().css("background", "#333333");
+    })
 })
